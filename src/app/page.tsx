33a652e7c1437a28aca4f10917b42901c47ff0c8
Helio Sales Jr <heliosalesjr@gmail.com>
2023-12-31 17:37:21 +0800

    posts slugs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,27 @@
+import fs from "fs";
+import Link from "../../node_modules/next/link";
 
+const getPostMetadata = () => {
+  const folder = "posts/";
+  const files = fs.readdirSync(folder);
+  const markdownPosts = files.filter((file) => file.endsWith(".md"));
+  const slugs = markdownPosts.map((file) => file.replace(".md", ""));
+  return slugs;
+};
 
 export default function Home() {
+  const postMetadata = getPostMetadata();
+  const postPreviews = postMetadata.map((slug) => 
+    <div>
+      <Link href={`/posts/${slug}`}>
+        <h2>{slug}</h2>
+      </Link>
+      
+    </div>
+  )
   return (
-    <>
-    <h1>Hey</h1>
-    <p>hey you</p>
-    </>
+    <div>
+      {postPreviews}
+    </div>
   )
 }
---
+++ src/app/posts/[slug]/page.tsx
@@ -0,0 +1,8 @@
+const PostPage = () => {
+    return(
+        <p>This is a post</p>
+    );
+
+};
+
+export default PostPage
